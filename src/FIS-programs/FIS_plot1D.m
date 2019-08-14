2ca59b6994dda52bf11ab4b2f8ca732155a30491
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2019-08-14 21:50:59 +0100

    added missing return values

--- src/FIS-programs/FIS_plot1D.m
+++ src/FIS-programs/FIS_plot1D.m
@@ -4,5 +4,12 @@ function [data, ax, mini, maxi] = FIS_plot1D()
   lambda = data(:,1);
   intensity = data(:,2);
   figure();
+  ax = axes();
   plot(lambda, intensity);
+
+  mini = min(intensity(:));
+  maxi = max(intensity(:));
+  if maxi >= 65535
+    warning('Saturated signal: maxi = %d', maxi);
+  end
 end
