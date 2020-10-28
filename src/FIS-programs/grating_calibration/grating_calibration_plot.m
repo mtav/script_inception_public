dd7822d56f45b73ef60c6ed834f03d4947f32dc2
mtav <Mike.Taverne@bristol.ac.uk>
2020-10-28 10:10:34 +0000

    Update grating_calibration_plot.m
    grating_calibration_plot() improved:
    -angle range and NA values can be passed as arguments
    -x/y axis labels added

--- src/FIS-programs/grating_calibration/grating_calibration_plot.m
+++ src/FIS-programs/grating_calibration/grating_calibration_plot.m
@@ -1,4 +1,4 @@
-function grating_calibration_plot(a_um, lambda_range)
+function grating_calibration_plot(a_um, lambda_range, angle_range_deg, NA_list)
 
   if exist('a_um','var')==0
     a_um = 1000/600;
@@ -6,6 +6,12 @@ function grating_calibration_plot(a_um, lambda_range)
 
   if exist('lambda_range','var')==0
     lambda_range = [0.899, 1.714];
+  end
+  if exist('angle_range_deg','var')==0
+    angle_range_deg = [-50, 50];
+  end
+  if exist('NA_list','var')==0
+    NA_list = [0.75, 1];
   end
 
   %  clear all;
@@ -25,7 +31,6 @@ function grating_calibration_plot(a_um, lambda_range)
 %    size(lambda_um)
   %  Xposition = FIS_PositionToAngle(theta_deg, 6, 1.88*10, false);
 
-  NA_list = [0.75, 1];
   LineSpec_list = {'r-', 'b-'};
   
   for idx = 1:length(NA_list)
@@ -47,8 +52,10 @@ function grating_calibration_plot(a_um, lambda_range)
       end
   end
 
-  xlim([-50, 50]);
+  xlim(angle_range_deg);
   ylim(lambda_range);
   set(gca, 'YDir','reverse');
   title(sprintf('a=%.2f um -> %d lines per mm', a_um, floor(1000/a_um)));
+  xlabel('Angle (degrees)');
+  ylabel('Wavelength (um)');
 end
