49cd130c8b2ef54c857e2989c6ed1f7200fafe84
mtav <Mike.Taverne@bristol.ac.uk>
2020-11-16 11:27:53 +0000

    grating_calibration_line.m: Added built-in conversion to real values.

--- src/FIS-programs/grating_calibration/grating_calibration_line.m
+++ src/FIS-programs/grating_calibration/grating_calibration_line.m
@@ -1,4 +1,5 @@
 function theta_deg = grating_calibration_line(m, NA, n, lambda_um, a_um)
     theta_rad = asin((NA/n)-(m*lambda_um/a_um));
     theta_deg = rad2deg(theta_rad);
+    theta_deg = real(theta_deg); % to avoid imaginary part warnings when plotting
 end
