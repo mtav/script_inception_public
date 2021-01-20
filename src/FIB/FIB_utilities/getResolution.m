7ebd533d1b9ce3921bc7a2e75ec5254a8684f06b
mtav <Mike.Taverne@bristol.ac.uk>
2021-01-20 10:49:58 +0000

    Improved FIB utility functions
    -Added FIB_infos() to get fundamental settings of the FIB. This could make it easier to support alternative FIB machines later on.
    -Utility functions improved, so they support array input.
    -Documentation and variable names improved.
    -FIB_beamDiameterStudy: Testing script that plots beam diameter in pixels as a function of magnitude and beamcurrent

--- src/FIB/FIB_utilities/getResolution.m
+++ src/FIB/FIB_utilities/getResolution.m
@@ -1,5 +1,20 @@
-function [res, HFW] = getResolution(mag)
-  % function [res, HFW] = getResolution(mag)
-  HFW = 304000/mag; % Width of the horizontal scan (mum).
-  res = HFW/4096; % size of each pixel (mum/pxl).
+function [resolution_mum_per_pxl, HFW_mum, FIB_infos_struct] = getResolution(mag)
+  % Usage:
+  %   [res, HFW, FIB_infos_struct] = getResolution(mag)
+  %
+  % Valid pixel ranges:
+  %   X: 0-4095 -> width: 4096 pixels
+  %   Y: 280-3816 -> height: 3537 pixels
+
+  FIB_infos_struct = FIB_infos();
+  
+  HFW_mum = FIB_infos_struct.HFW_at_mag1_in_mum ./ mag; % Width of the horizontal scan (mum).
+  resolution_mum_per_pxl = HFW_mum ./ FIB_infos_struct.Npixels_X; % size of each pixel (mum/pxl).
+  
+  FIB_infos_struct.mag = mag;
+  FIB_infos_struct.resolution_mum_per_pxl = resolution_mum_per_pxl;
+  
+  FIB_infos_struct.size_X_mum = HFW_mum;
+  FIB_infos_struct.size_Y_mum = FIB_infos_struct.Npixels_Y .* FIB_infos_struct.resolution_mum_per_pxl;
+  
 end
