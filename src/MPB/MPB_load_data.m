6c215179db99f748a5814e2d825aad9833d32140
mtav <Mike.Taverne@bristol.ac.uk>
2021-08-10 14:01:44 +0100

    Adding MPB_load_data script.

---
+++ src/MPB/MPB_load_data.m
@@ -0,0 +1,24 @@
+function s = MPB_load_data(infile)
+  if ~exist('infile', 'var')
+    [infile_name, infile_dir] = uigetfile('*.dat');
+    infile = fullfile(infile_dir, infile_name);
+  end
+  
+  s = struct();
+  s.infile = infile;
+  [s.infile_dir, s.infile_base, s.infile_ext] = fileparts(s.infile);
+  
+  data = readmatrix(s.infile, 'Range', [2,2]);
+  
+  s.kindex = data(:,1);
+  s.kindex_fixed = 1:size(data, 1);
+  s.k1 = data(:,2);
+  s.k2 = data(:,3);
+  s.k3 = data(:,4);
+  s.kmag = data(:,5);
+  
+  for i = 6:size(data,2)
+    fn = data(:,i);
+    s.fn(:,i-5) = fn(:);
+  end
+end
