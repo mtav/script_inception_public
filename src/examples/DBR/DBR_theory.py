2a35c6f1c4334f887f4b786dd1cb7db03a3a5f08
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2022-08-18 11:13:04 +0100

    MPB plot WIP

--- src/examples/DBR/DBR_theory.py
+++ src/examples/DBR/DBR_theory.py
@@ -29,6 +29,21 @@ class DBR():
     t1 = n2/(n1+n2)
     t2 = n1/(n1+n2)
 
+    @property
+    def a(self): return self.getPeriod()
+
+    def getBraggFrequency(self):
+        '''
+        Returns omega_{B}=(c0/navg)*(pi/a)
+        where:
+            navg = (n1*t1+n2*t2)/a
+        '''
+        return (get_c0()/self.getAverageRefractiveIndex()) * (np.pi/self.getPeriod())
+
+    def getAverageRefractiveIndex(self):
+        return (self.n1*self.t1 + self.n2*self.t2) / self.getPeriod()
+        
+
     def getBrewsterAngles(self, degrees=False):
         thetaB_1_rad = np.arctan(self.n2/self.n1)
         thetaB_2_rad = np.arctan(self.n1/self.n2)
@@ -1235,6 +1250,141 @@ def testNanxyArrays():
     x,y=np.meshgrid(x,y)
     z = x+y
     plt.pcolormesh(x,y,z)
+
+def plotMPB():
+    
+    dbr = DBR()
+    dbr.n1 = 1.5
+    dbr.n2 = 3.5
+    dbr.t1 = 0.5
+    dbr.t2 = 0.5
+    
+    pr = plottingRange(beta_normalized=np.linspace(-0.5,0.5),
+                       fn=np.linspace(0,0.30),
+                       lattice_constant=dbr.a)
+    
+    import math
+    import meep as mp
+    from meep import mpb
+    import matplotlib.pyplot as plt
+
+    geometry_lattice_1D = mp.Lattice(size=mp.Vector3(1, 0, 0))
+    geometry_lattice_2D = mp.Lattice(size=mp.Vector3(1, 1, 0))
+                                  # basis_size=mp.Vector3(1, 1),
+                                  # basis1=mp.Vector3(math.sqrt(3)/2, 0.5),
+                                  # basis2=mp.Vector3(math.sqrt(3)/2, -0.5))
+    
+    num_bands = 8
+    resolution = 32
+    # geometry_lattice = mp.Lattice(size=mp.Vector3(1, 1),
+    #                               basis1=mp.Vector3(math.sqrt(3)/2, 0.5),
+    #                               basis2=mp.Vector3(math.sqrt(3)/2, -0.5))
+    # geometry = [mp.Cylinder(0.2, material=mp.Medium(epsilon=12))]
+
+    # sqrt_half = math.sqrt(0.5)
+    # geometry_lattice = mp.Lattice(
+    #     basis_size=mp.Vector3(sqrt_half, sqrt_half, sqrt_half),
+    #     basis1=mp.Vector3(0, 1, 1),
+    #     basis2=mp.Vector3(1, 0, 1),
+    #     basis3=mp.Vector3(1, 1)
+    # )    
+    
+    block1 = mp.Block(center=mp.Vector3(-dbr.a/2+dbr.t1/2),
+             size=mp.Vector3(dbr.t1, mp.inf, mp.inf),
+             material=mp.Medium(index=dbr.n1))
+
+    block2 = mp.Block(center=mp.Vector3(dbr.t1/2),
+             size=mp.Vector3(dbr.t2, mp.inf, mp.inf),
+             material=mp.Medium(index=dbr.n2))
+    
+    geometry = [block1, block2]
+    
+    # k_points = [
+    #     mp.Vector3(),               # Gamma
+    #     mp.Vector3(y=0.5),          # M
+    #     mp.Vector3(-1./3, 1./3),    # K
+    #     mp.Vector3(),               # Gamma
+    # ]
+    k_points = [
+        mp.Vector3(-0.5),          # M
+        mp.Vector3(),               # Gamma
+        mp.Vector3(0.5),          # M
+    ]
+    k_points = mp.interpolate(10, k_points)
+
+    ms = mpb.ModeSolver(
+        geometry = geometry,
+        geometry_lattice = geometry_lattice_2D,
+        k_points = k_points,
+        resolution = resolution,
+        num_bands = num_bands
+    )
+    ms.init_params(p=mp.NO_PARITY, reset_fields=True)
+    md = mpb.MPBData(rectify=True, periods=1, resolution=32)
+    eps = ms.get_epsilon()
+    converted_eps = md.convert(eps)
+    plt.imshow(converted_eps.T)#, interpolation='spline36', cmap='binary')
+    plt.colorbar()
+    plt.axis('off')
+    
+    plt.show()
+    
+    ms = mpb.ModeSolver(
+        geometry = geometry,
+        geometry_lattice = geometry_lattice_1D,
+        k_points = k_points,
+        resolution = resolution,
+        num_bands = num_bands
+    )
+    ms.run_tm()
+    # ms.run_tm(mpb.output_at_kpoint(mp.Vector3(-1./3, 1./3), mpb.fix_efield_phase,
+    #           mpb.output_efield_z))
+    tm_freqs = ms.all_freqs
+    tm_gaps = ms.gap_list
+    ms.run_te()
+    te_freqs = ms.all_freqs
+    te_gaps = ms.gap_list
+    
+    x = [k[0] for k in ms.k_points]
+    plt.plot(x, te_freqs*(2*np.pi*get_c0()/dbr.a)/dbr.getBraggFrequency() )
+    plt.ylim([0,2.5])
+    return
+    
+    fig, ax = plt.subplots()
+    x = range(len(tm_freqs))
+    # Plot bands
+    # Scatter plot for multiple y values, see https://stackoverflow.com/a/34280815/2261298
+    for xz, tmz, tez in zip(x, tm_freqs, te_freqs):
+        ax.scatter([xz]*len(tmz), tmz, color='blue')
+        ax.scatter([xz]*len(tez), tez, color='red', facecolors='none')
+    ax.plot(tm_freqs, color='blue')
+    ax.plot(te_freqs, color='red')
+    ax.set_ylim([0, 1])
+    ax.set_xlim([x[0], x[-1]])
+    
+    # Plot gaps
+    for gap in tm_gaps:
+        if gap[0] > 1:
+            ax.fill_between(x, gap[1], gap[2], color='blue', alpha=0.2)
+    
+    for gap in te_gaps:
+        if gap[0] > 1:
+            ax.fill_between(x, gap[1], gap[2], color='red', alpha=0.2)
+    
+    
+    # Plot labels
+    ax.text(12, 0.04, 'TM bands', color='blue', size=15)
+    ax.text(13.05, 0.235, 'TE bands', color='red', size=15)
+    
+    points_in_between = (len(tm_freqs) - 4) / 3
+    tick_locs = [i*points_in_between+i for i in range(4)]
+    tick_labs = ['Γ', 'X', 'M', 'Γ']
+    ax.set_xticks(tick_locs)
+    ax.set_xticklabels(tick_labs, size=16)
+    ax.set_ylabel('frequency (c/a)', size=16)
+    ax.grid(True)
+    
+    plt.show()
     
 def main():
     # test_plottingRange()
@@ -1245,9 +1395,10 @@ def main():
     # test_findBandEdges_v1()
     # test_findBandEdges_v2()
     # test_reference_DBR_3()
-    testTMM()
+    # testTMM()
     # testParallelPython()
     # testNanxyArrays()
+    plotMPB()
 
     plt.show()
     print('SUCCESS')
