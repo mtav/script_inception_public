e0dabf653db6b4149c0243c5bcf8c796e67a4547
Mike Taverne <352716+mtav@users.noreply.github.com>
2023-03-17 12:38:22 +0000

    zoomPlot: Fixed problem in case of multiple find matches. + Added documentation.

--- src/utilities/zoomPlot.m
+++ src/utilities/zoomPlot.m
@@ -1,8 +1,17 @@
-function [xzoom, yzoom]=zoomPlot(x, y, xmin, xmax)
+function [xzoom, yzoom, index_min, index_max] = zoomPlot(x, y, xmin, xmax)
+  % function [xzoom, yzoom, index_min, index_max] = zoomPlot(x, y, xmin, xmax)
+  %   Returns a subset xzoom, yzoom of x,y so that:
+  %     x(index_min) is closest to xmin
+  %     x(index_max) is closest to xmax
+  %   For 1D plots only!
   index_1 = find(abs(x-xmin)==min(abs(x-xmin)));
   index_2 = find(abs(x-xmax)==min(abs(x-xmax)));
-  index_min = min(index_1,index_2);
-  index_max = max(index_1,index_2);
+  
+  edge_indices = [index_1(:); index_2(:)];
+  
+  index_min = min(edge_indices(:));
+  index_max = max(edge_indices(:));
+  
   xzoom = x(index_min:index_max);
   yzoom = y(index_min:index_max);
 end
